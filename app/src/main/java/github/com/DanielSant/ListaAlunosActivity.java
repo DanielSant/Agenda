625e611ed592074ec60e29bc9f2694b8a8ee6312
Daniel Santos <daniel17sants@gmail.com>
2020-01-23 17:41:26 -0300

    Iniciando projeto de uma agenda

---
+++ app/src/main/java/github/com/DanielSant/ListaAlunosActivity.java
@@ -0,0 +1,14 @@
+package github.com.DanielSant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+    }
+}
